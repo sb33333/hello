fa5858404e3a1a62c2c10dd65a354684b6541471
MUNA-desktop <springbean3@outlook.kr>
2023-01-24 18:46:18 +0900

    first commit

---
+++ src/test/java/home/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package home.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
